1be4ff3ee72cf644d75eb625a82ac761c5cff5ee
Adam Whittingham <adam.whittingham@gmail.com>
2015-04-04 12:22:00 +0100

    Throw github.js in and see what it looks like

---
+++ js/src/main.js
@@ -0,0 +1,14 @@
+Github.orgProfile({
+  orgname: 'dvmtn',
+  selector: '#dvmtn'
+});
+
+Github.userProfile({
+  username: 'atleastimtrying',
+  selector: '#anders'
+});
+
+Github.userProfile({
+  username: 'adamwhittingham',
+  selector: '#adam'
+});
